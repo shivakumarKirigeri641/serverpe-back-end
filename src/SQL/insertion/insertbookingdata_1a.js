26204136a3c473677e5fa0ee4680ed53630ce652
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-10-25 13:11:53 +0530

    task:1a included.

---
+++ src/SQL/insertion/insertbookingdata_1a.js
@@ -0,0 +1,271 @@
+const getFareDetails = require("../fetchers/getFareDetails");
+const insertbookingdata_1a = async (client, booking_details) => {
+  let booked_details = null;
+  let passenger_details = [];
+  let search_details = {};
+  try {
+    //check train
+    const result_train_number = await client.query(
+      `select id from coaches where train_number = $1`,
+      [booking_details.train_number]
+    );
+    //check if coach given is applicable for selected train?
+    const result_coach_check = await client.query(
+      `select a_1 from coaches where train_number = $1`,
+      [booking_details.train_number]
+    );
+    if (0 < result_coach_check.rows.length) {
+      if ("N" === result_coach_check.rows[0].a_1.toUpperCase()) {
+        throw {
+          status: 200,
+          success: false,
+          message: "Coach not applicable for selected train!",
+        };
+      }
+    } else {
+      throw {
+        status: 200,
+        success: false,
+        message: "Invalid coach found!",
+      };
+    }
+    if (0 === result_train_number.rows.length) {
+      throw {
+        status: 400,
+        message: `Selected train number ${booking_details.train_number} not found!`,
+        data: {},
+      };
+    }
+    //check res_type
+    const result_reservation_type = await client.query(
+      `select id from reservationtype where type_code = $1`,
+      [booking_details.reservation_type]
+    );
+    if (0 === result_reservation_type.rows.length) {
+      throw {
+        status: 400,
+        message: `Selected reservation type ${booking_details.reservation_type} not found!`,
+        data: {},
+      };
+    }
+    //check coach_type
+    const result_coach_type = await client.query(
+      `select id from coachtype where coach_code = $1`,
+      [booking_details.coach_type]
+    );
+    if (0 === result_coach_type.rows.length) {
+      throw {
+        status: 400,
+        message: `Selected coach type ${booking_details.coach_code} not found!`,
+        data: {},
+      };
+    }
+    //src exists
+    const result_src = await client.query(
+      `select id from stations where code = $1`,
+      [booking_details.source_code.toUpperCase()]
+    );
+    if (0 === result_src.rows.length) {
+      throw {
+        status: 400,
+        message: `Source not found!`,
+        data: {},
+      };
+    }
+    //dest exists
+    const result_dest = await client.query(
+      `select id from stations where code = $1`,
+      [booking_details.destination_code.toUpperCase()]
+    );
+    if (0 === result_dest.rows.length) {
+      throw {
+        status: 400,
+        message: `Destination ${booking_details.destination_code} not found!`,
+        data: {},
+      };
+    }
+    //boarding_at
+    const result_brdingat = await client.query(
+      `select id from stations where code = $1`,
+      [booking_details.boarding_at.toUpperCase()]
+    );
+    if (0 === result_brdingat.rows.length) {
+      throw {
+        status: 400,
+        message: `Boarding point ${booking_details.boarding_at} not found!`,
+        data: {},
+      };
+    }
+    if (!booking_details.passenger_details) {
+      throw {
+        status: 400,
+        message: `Passenger list found!`,
+        data: {},
+      };
+    }
+    let adult_count = booking_details.passenger_details.filter(
+      (x) => x.passenger_ischild === false || x.passenger_issenior === true
+    );
+    let child_count = booking_details.passenger_details.filter(
+      (x) => x.passenger_ischild === true
+    );
+    if (
+      !booking_details.passenger_details ||
+      (0 === adult_count.length && 0 === child_count.length)
+    ) {
+      throw {
+        status: 400,
+        message: `Invalid passenger details found!`,
+        data: {},
+      };
+    }
+    //lock advicery loack
+    await client.query(
+      `SELECT hashtext(mobile_number) FROM bookingdata WHERE mobile_number = $1`,
+      [booking_details.mobile_number]
+    );
+    //first check if user is already logged in but no booked (like check trains->again go back & modify src,dest date etc)
+    const result_is_already_user = await client.query(
+      `select *from bookingdata where mobile_number = $1 and proceed_status=$2`,
+      [booking_details.mobile_number, false]
+    );
+    if (0 === result_is_already_user.rows.length) {
+      booked_details = await client.query(
+        `insert into bookingdata (fktrain_number, date_of_journey, fksource_code, fkdestination_code, fkreservation_type, fkcoach_type, mobile_number, adult_count, child_count, fkboarding_at) values (
+      $1,$2,$3,$4,$5,$6,$7,$8,$9,$10) returning *`,
+        [
+          result_train_number.rows[0].id,
+          booking_details.doj,
+          result_src.rows[0].id,
+          result_dest.rows[0].id,
+          result_reservation_type.rows[0].id,
+          result_coach_type.rows[0].id,
+          booking_details.mobile_number,
+          adult_count.length,
+          child_count.length,
+          result_brdingat.rows[0].id,
+        ]
+      );
+      //insert passenger list
+      for (let i = 0; i < booking_details.passenger_details.length; i++) {
+        const temp = await client.query(
+          `insert into passengerdata (fkbookingdata,p_name, p_age, p_gender, preferred_berth, is_child, is_senior, is_adult) values ($1,$2,$3,$4,$5,$6,$7,$8) returning *`,
+          [
+            booked_details.rows[0].id,
+            booking_details.passenger_details[i].passenger_name,
+            booking_details.passenger_details[i].passenger_age,
+            booking_details.passenger_details[i].passenger_gender,
+            booking_details.passenger_details[i].preferred_berth
+              ? booking_details.passenger_details[i].preferred_berth
+              : null,
+            booking_details.passenger_details[i].passenger_age < 6
+              ? true
+              : false,
+            booking_details.passenger_details[
+              i
+            ].passenger_gender.toUpperCase() === "F"
+              ? booking_details.passenger_details[i].passenger_age > 50
+                ? true
+                : false
+              : booking_details.passenger_details[i].passenger_age > 60
+              ? true
+              : false,
+            !(booking_details.passenger_details[
+              i
+            ].passenger_gender.toUpperCase() === "F"
+              ? booking_details.passenger_details[i].passenger_age > 50
+                ? true
+                : false
+              : booking_details.passenger_details[i].passenger_age > 60
+              ? true
+              : false),
+          ]
+        );
+        passenger_details.push(temp.rows[0]);
+      }
+      //now get fare deatils:
+      const fare_details = await getFareDetails(
+        client,
+        booking_details,
+        booked_details,
+        passenger_details
+      );
+      return {
+        booked_details: booked_details.rows[0],
+        passenger_details: passenger_details,
+        fare_details: fare_details,
+      };
+    } else {
+      //update coach
+      await client.query(
+        `update bookingdata set fktrain_number=$1, fksource_code=$2, fkdestination_code=$3, fkboarding_at=$4, fkcoach_type=$5, mobile_number=$6, adult_count=$7, child_count=$8 where id=$9`,
+        [
+          result_train_number.rows[0].id,
+          result_src.rows[0].id,
+          result_dest.rows[0].id,
+          result_brdingat.rows[0].id,
+          result_coach_type.rows[0].id,
+          booking_details.mobile_number,
+          adult_count.length,
+          child_count.length,
+          result_is_already_user.rows[0].id,
+        ]
+      );
+      //insert passenger list
+      await client.query(`delete from passengerdata where fkbookingdata = $1`, [
+        result_is_already_user.rows[0].id,
+      ]);
+      for (let i = 0; i < booking_details.passenger_details.length; i++) {
+        const temp = await client.query(
+          `insert into passengerdata (fkbookingdata,p_name, p_age, p_gender, preferred_berth, is_child, is_senior, is_adult) values ($1,$2,$3,$4,$5,$6,$7,$8) returning *`,
+          [
+            result_is_already_user.rows[0].id,
+            booking_details.passenger_details[i].passenger_name,
+            booking_details.passenger_details[i].passenger_age,
+            booking_details.passenger_details[i].passenger_gender,
+            booking_details.passenger_details[i].preferred_berth
+              ? booking_details.passenger_details[i].preferred_berth
+              : null,
+            booking_details.passenger_details[i].passenger_age < 6
+              ? true
+              : false,
+            booking_details.passenger_details[
+              i
+            ].passenger_gender.toUpperCase() === "F"
+              ? booking_details.passenger_details[i].passenger_age > 50
+                ? true
+                : false
+              : booking_details.passenger_details[i].passenger_age > 60
+              ? true
+              : false,
+            !(booking_details.passenger_details[
+              i
+            ].passenger_gender.toUpperCase() === "F"
+              ? booking_details.passenger_details[i].passenger_age > 50
+                ? true
+                : false
+              : booking_details.passenger_details[i].passenger_age > 60
+              ? true
+              : false),
+          ]
+        );
+        passenger_details.push(temp.rows[0]);
+      }
+      //now get fare deatils:
+      const fare_details = await getFareDetails(
+        client,
+        booking_details,
+        booked_details,
+        passenger_details
+      );
+      return {
+        booked_details: result_is_already_user.rows[0],
+        passenger_details: passenger_details,
+        fare_details: fare_details,
+      };
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+module.exports = insertbookingdata_1a;
---
+++ src/SQL/reservations/book_general_1a.js
@@ -0,0 +1,101 @@
+const axios = require("axios");
+const allocateSeat_1a = require("../../utils/allocateSeat_1a");
+require("dotenv").config();
+const getPnrNumber = require("../../utils/getPnrNumber");
+const book_general_1a = async (
+  client,
+  result_details,
+  passengerdetails,
+  booking_id
+) => {
+  let result_updated_bookingdetails = {};
+  let result_udpated_passengerdetails = [];
+  let seat_allocation_status = "CNF";
+  const pnr = getPnrNumber(booking_id);
+  //fetch scheduled departure
+  const result_scheduled_departure = await client.query(
+    `select departure from schedules where train_number=$1 and station_code = $2`,
+    [result_details.rows[0].train_number, result_details.rows[0].source_code]
+  );
+  for (let i = 0; i < passengerdetails.rows.length; i++) {
+    //lock
+    let result_seats_availability = await client.query(
+      `select *from seatsondate_1a where train_number=$1 and date_of_journey=$2 for update`,
+      [
+        result_details.rows[0].train_number,
+        result_details.rows[0].date_of_journey,
+      ]
+    );
+    //lock bookingdata
+    let result_bookingdata = await client.query(
+      `select *from bookingdata where id=$1 for update`,
+      [booking_id]
+    );
+    //adivicerylock (LETS SEE HOW IT WORKS THEN DO THIS)
+
+    //check if gen_seats are there
+    if (0 < result_seats_availability.rows[0].gen_count) {
+      seat_allocation_status = "CNF";
+      let current_gen_seat = result_seats_availability.rows[0].gen_count;
+      //decrement the count of gen_seats
+      await client.query(
+        `update seatsondate_1a set gen_count = $1, total_seats=$2 where id=$3`,
+        [
+          --result_seats_availability.rows[0].gen_count,
+          --result_seats_availability.rows[0].total_seats,
+          result_seats_availability.rows[0].id,
+        ]
+      );
+      //insert into seat alloation
+      await client.query(
+        `insert into seatallocation_1a (fkpassengerdata, fk_seatsondate_1a, seat_sequence_number, seat_status)values ($1,$2,$3,$4) returning *`,
+        [
+          passengerdetails.rows[i].id,
+          result_seats_availability.rows[0].id,
+          current_gen_seat,
+          "CNF",
+        ]
+      );
+      //update the passengerdetails
+      const temp = await client.query(
+        `update passengerdata set seat_status=$1, current_seat_status=$2, updated_seat_status=$3 where id=$4 returning *`,
+        ["CNF", "CNF", "CNF", passengerdetails.rows[i].id]
+      );
+      result_udpated_passengerdetails.push(temp.rows[0]);
+    }
+  }
+  //update bookingid with pnr
+  result_updated_bookingdetails = await client.query(
+    `update bookingdata set pnr=$1, pnr_status=$2, proceed_status=$3 where id =$4 returning *`,
+    [pnr, seat_allocation_status, true, booking_id]
+  );
+  //send sms
+  /*const fast2smsResp = await axios.get(
+    `https://www.fast2sms.com/dev/bulkV2?authorization=${
+      process.env.FAST2SMSAPIKEY
+    }&route=dlt&sender_id=NOQPNR&message=200941&variables_values=${pnr}|${
+      result_details.rows[0].train_number +
+      " (" +
+      result_details.rows[0].source_code +
+      " - " +
+      result_details.rows[0].destination_code +
+      ")"
+    }|${
+      result_details.rows[0].date_of_journey +
+      ", " +
+      result_scheduled_departure.rows[0].departure
+    }&numbers=${result_details.rows[0].mobile_number}`
+  );
+  if (fast2smsResp.data && fast2smsResp.data.return) {
+    //dont do anything
+    console.log(fast2smsResp.data);
+  } else {
+    // bubble details for debugging
+    console.log(fast2smsResp.data);
+  }*/
+  return {
+    result_updated_bookingdetails: result_updated_bookingdetails.rows[0],
+    result_udpated_passengerdetails,
+  };
+};
+module.exports = book_general_1a;
