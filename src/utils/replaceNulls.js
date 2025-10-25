d027253c3fab8c13c1d8a9f225930415044a1a23
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-10-25 11:41:30 +0530

    task:replace with '-' for null

---
+++ src/utils/replaceNulls.js
@@ -0,0 +1,17 @@
+const replaceNulls = (obj) => {
+  if (Array.isArray(obj)) {
+    return obj.map(replaceNulls);
+  } else if (obj !== null && typeof obj === "object") {
+    const newObj = {};
+    for (const key in obj) {
+      newObj[key] = replaceNulls(obj[key]);
+    }
+    return newObj;
+  } else if (obj === null) {
+    //return "NOT AVAILABLE";
+    return "-";
+  } else {
+    return obj;
+  }
+};
+module.exports = replaceNulls;
