f09f6bc3a8a2562e1648410da04658121e4b6355
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-11-25 17:59:14 +0530

    task:fix for train number

--- src/SQL/proceedBooking.js
+++ src/SQL/proceedBooking.js
@@ -363,8 +363,9 @@ where s1.station_code = $1 and s2.station_code = $2 and s1.station_sequence >=s2
         };
     }
     const updated_booked_details = await await client.query(
-      `select b.*, t.train_number, c.coach_code, r.type_code, src.code, src.station_name, dest.code, dest.station_name, brding.code, brding.station_name  from bookingdata b
-left join trains t on t.id = b.fktrain_number
+      `select b.*, t.train_number as train_number, c.coach_code, r.type_code, src.code as source_code, src.station_name as source_name, dest.code as destination_code, dest.station_name as destination_name
+      , brding.code as boarding_point, brding.station_name as boarding_point_name from bookingdata b
+left join coaches t on t.id = b.fktrain_number
 left join coachtype c on c.id = b.fkcoach_type
 left join reservationtype r on r.id = b.fkreservation_type
 left join stations src on src.id = b.fksource_code
