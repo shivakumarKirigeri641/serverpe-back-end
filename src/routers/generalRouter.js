fb64d3eb998586f3f4a6ed23c0796e4c28653a4a
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-12-26 18:27:44 +0530

    task:missing text

--- src/routers/generalRouter.js
+++ src/routers/generalRouter.js
@@ -78,7 +78,7 @@ generalRouter.post("/mockapis/serverpeuser/verify-otp", async (req, res) => {
       );
       if (validateforverifyotpresult.successstatus) {
         const token = generateToken(req.body.mobile_number);
-        res.cookie("serverpe_user_token", token, {
+        res.cookie("token", token, {
           httpOnly: true,
           secure: true, // REQUIRED for SameSite=None
           sameSite: "none", // REQUIRED for cross-domain React → Node
