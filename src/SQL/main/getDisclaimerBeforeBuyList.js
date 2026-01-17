9360b3bd0f7cd0aff35d94fa5eb5a760dc262ba5
Shivakumar Kirigeri <shivakumar641@gmail.com>
2026-01-17 19:40:24 +0530

    task:disclaimer update & invoice update

--- src/SQL/main/getDisclaimerBeforeBuyList.js
+++ src/SQL/main/getDisclaimerBeforeBuyList.js
@@ -1,13 +1,14 @@
-
 const getDisclaimerBeforeBuyList = async (client, req) => {
   const result = await client.query(
-    `select *from disclaimer_before_buy order by title_id;`
+    `SELECT id, title_id, title, description, is_mandatory, is_active 
+     FROM disclaimer_before_buy 
+     WHERE is_active = true;`,
   );
   return {
     statuscode: 200,
     successstatus: true,
     data: result.rows,
-    message: "project details fetched successfully.",
+    message: "Disclaimers fetched successfully.",
   };
 };
 module.exports = getDisclaimerBeforeBuyList;
