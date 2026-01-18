0fb47674724cf9aca0cfd8d678993a3c482a6bb4
Shivakumar Kirigeri <shivakumar641@gmail.com>
2026-01-18 08:45:28 +0530

    task:issue fix for ticket sending sms

--- src/utils/sendConfirmTicketSMS.js
+++ src/utils/sendConfirmTicketSMS.js
@@ -6,11 +6,11 @@ const sendConfirmTicketSMS = async (
   train_number,
   train_name,
   date_of_journey,
-  departure
+  departure,
 ) => {
   const fast2smsResp = await axios.get(
     //`https://www.fast2sms.com/dev/bulkV2?authorization=${process.env.FAST2SMSAPIKEY}&route=dlt&sender_id=SRVRPE&message=204637&variables_values=${generatedotp}|${validfor}&numbers=${mobile_number}`
-    `https://www.fast2sms.com/dev/bulkV2?authorization=${process.env.FAST2SMSAPIKEY}&route=dlt&sender_id=NOQPNR&message=200941&variables_values=${pnr}|#${train_number} , ${train_name}|${date_of_journey}, ${departure}&numbers=${mobile_number}`
+    `https://www.fast2sms.com/dev/bulkV2?authorization=${process.env.FAST2SMSAPIKEY}&route=dlt&sender_id=NOQPNR&message=200941&variables_values=${pnr}|${train_number} , ${train_name}|${date_of_journey}, ${departure}&numbers=${mobile_number}`,
   );
   if (fast2smsResp.data && fast2smsResp.data.return) {
     //dont do anything
