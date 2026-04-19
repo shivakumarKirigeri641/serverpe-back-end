63471aa19d5db5a504a69c1ac94079568defed7d
Shivakumar Kirigeri <shivakumar641@gmail.com>
2026-04-19 17:32:13 +0530

    task:serverpe back get apis

--- src/routers/publicRouter.js
+++ src/routers/publicRouter.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const publicRouter = express.Router();
 const getQueryTypes = require("../repos/gets/getQueryTypes");
+const getPrivacyPolicy = require("../repos/gets/getPrivacyPolicy");
 const {
   authLimiter,
   sensitiveOpLimiter,
 } = require("../middlewares/rateLimiter");
 const validateForContactUs = require("../validators/validateForContactUs");
 const insertContactQuery = require("../repos/insertions/insertContactQuery");
+const getTermsAndConditions = require("../repos/gets/getTermsAndConditions");
 // ======================================================
 //                QUERY-TYPES
 // ======================================================
@@ -70,6 +72,56 @@ publicRouter.post(
         message: `Internal server error. Error:${err.message}`,
       });
     } finally {
+    }
+  },
+);
+// ======================================================
+//                PRIVACY-POLICY
+// ======================================================
+publicRouter.get(
+  "/serverpe/platform/public/privacy-policy",
+  async (req, res) => {
+    try {
+      const result = await getPrivacyPolicy();
+      return res.status(result.statuscode).json({
+        statuscode: result.statuscode,
+        powered_by: "ServerPe App Solutions",
+        successstatus: result.successstatus,
+        message: result.message,
+        data: result?.data,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        error: "Internal Server Error",
+        successstatus: false,
+        message: err.message,
+      });
+    }
+  },
+);
+// ======================================================
+//                TERMS-AND-CONDITIONS
+// ======================================================
+publicRouter.get(
+  "/serverpe/platform/public/terms-and-conditions",
+  async (req, res) => {
+    try {
+      const result = await getTermsAndConditions();
+      return res.status(result.statuscode).json({
+        statuscode: result.statuscode,
+        powered_by: "ServerPe App Solutions",
+        successstatus: result.successstatus,
+        message: result.message,
+        data: result?.data,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        error: "Internal Server Error",
+        successstatus: false,
+        message: err.message,
+      });
     }
   },
 );
